01c84a060a932f35de3284736962cd276a777a70
Rory Sie <rory.sie@ortec.com>
2020-06-29 17:17:08 +0200

    fixes post gen dir removal

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,14 +8,18 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def remove_dir(dir_path):
+    os.rmdir(os.path.join(PROJECT_DIRECTORY, dir_path)
+
+
 if __name__ == '__main__':
     if '{{ cookiecutter.packaging }}' != 'pip':
         remove_file('requirements.txt')
     if '{{ cookiecutter.packaging }}' != 'conda':
         remove_file('environment.yml')
     if '{{ cookiecutter.image_recognition }}' != 'y':
-        remove_file('resources/train')
-        remove_file('resources/test')
+        remove_dir('resources/train')
+        remove_dir('resources/test')
     if '{{ cookiecutter.manual_Docker_containers }}' != 'y':
         remove_file('Dockerfile')
         remove_file('entrypoint.sh')
